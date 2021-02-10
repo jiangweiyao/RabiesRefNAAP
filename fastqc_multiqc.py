4f4834284b10b8fdfee912d68e312d6cee17610e
Yao <pqq7@login02.aspen.biotech.cdc.gov>
2021-02-10 17:10:04 -0500

    first message

---
+++ fastqc_multiqc.py
@@ -0,0 +1,40 @@
+import sys
+import glob
+import os
+import subprocess
+import argparse
+import re
+
+def main():
+    
+    local_path = os.path.dirname(os.path.realpath(__file__))
+
+    cli = argparse.ArgumentParser()
+    cli.add_argument('-i', '--InputFolder', help="Input folder file", required=True)
+    cli.add_argument('-o', '--OutputFolder', help="Output folder", required=False, default=".")
+    args = cli.parse_args()
+    #subprocess.run(["wc", args.Input])
+
+    files = sorted([f for f in glob.glob(args.InputFolder+"/**", recursive = True) if re.search(r'(.*)\.((fastq|fq)(|\.gz))$', f)])   
+    #InputFolder = os.path.expanduser(args.InputFolder)
+    #files = sorted(glob.glob(InputFolder+"/*.fastq"))
+    print(files)
+
+    OutputFolder = os.path.expanduser(args.OutputFolder)
+    os.system(f"mkdir -p {OutputFolder}")
+
+
+    for i in range(0, len(files)):
+        filec = files[i]
+
+        base = os.path.splitext(os.path.basename(filec))[0]
+        base = os.path.splitext(base)[0]
+        #print(base)
+        #subprocess.call(["seqtk", "seq", "-L", str(args.Size), args.Input], stdout=f)
+        subprocess.call(["fastqc", filec, "-o", OutputFolder])
+
+
+    subprocess.call(["multiqc", OutputFolder, "-o", OutputFolder])
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ seqtk_sizefilter_trim.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+import argparse 
+
+
+def main():
+
+    cli = argparse.ArgumentParser()
+    cli.add_argument('-i', '--Input', help="Input fastq file", required=True)
+    cli.add_argument('-o', '--Output', help="Output file name", required=False, default="SizeFilteredAndTrimmed.fastq")
+    cli.add_argument('-s', '--Size', help="Filter out reads shorter than size", required=False, default=50)
+    cli.add_argument('-l', '--Left', help = "Trim this many bases from the left size", required=False, default=25)
+    cli.add_argument('-r', '--Right', help = "Trim this many bases from the right size", required=False, default=25)
+    args = cli.parse_args()
+    #subprocess.run(["wc", args.Input])
+
+    f = open("filteredintaaaaaa.fq", "w")
+    subprocess.call(["seqtk", "seq", "-L", str(args.Size), args.Input], stdout=f)
+    f.close()
+
+    f = open(args.Output, "w")
+    subprocess.call(["seqtk", "trimfq", "-b", str(args.Left), "-e", str(args.Right), "filteredintaaaaaa.fq"], stdout=f)
+    f.close()
+
+    os.remove("filteredintaaaaaa.fq")
+ 
+if __name__ == "__main__":
+    sys.exit(main())
