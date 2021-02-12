a133f6261dd2e65b8e73b4268fdffb7ae51d2a23
Jiangwei Yao <yao.jiangwei@example.com>
2021-02-12 14:41:57 -0500

    v5

--- refnaap_cli.py
+++ refnaap_cli.py
@@ -13,13 +13,12 @@ def main():
 
     cli = argparse.ArgumentParser()
     cli.add_argument('-i', '--Input', help="Input file", required=True)
-    cli.add_argument('-o', '--Output', help="Output", required=False, default="contigs.fasta")
+    cli.add_argument('-o', '--Output', help="OutputFolder", required=False, default="output1/")
     cli.add_argument('-r', '--RefFile', help="Reference File ", required=False, default=f'{local_path}/Americas2.fasta')
     cli.add_argument('--TopN', help="The top N reference sequences with the most depth are analyzed.", type=int, required=False, default=1)
     cli.add_argument('--MinCov', help="Amplicon regions need a minimum of this average coverage number", type=int, required=False, default=5)
     cli.add_argument('-t', '--threads', help="Number of threads. More is faster if your computer supports it", type=int, required=False, default=4)
     cli.add_argument('--model', help="Basecall Model", required=False, type=str, default='r10_min_high_g303')
-    cli.add_argument('--verbose', help = "Keep Intermediate Files", required=False, default=False)
     args = cli.parse_args()
 
 
@@ -44,6 +43,18 @@ def main():
 
     subprocess.call(["Rscript", gapfixer_helper, OutputFolder+"/scaffold.fasta", OutputFolder+"/medaka1/consensus.fasta", OutputFolder+"/gapfixed.fasta"])
 
+    subprocess.call(["medaka_consensus", "-i", str(args.Input), "-d", OutputFolder+"/gapfixed.fasta", "-o", OutputFolder+"/medaka2", "-m", str(args.model), "-t", str(args.threads)])
+
+
+    bcf_pileup_cmd = subprocess.Popen(['bcftools', 'mpileup', '-f', OutputFolder+"/gapfixed.fasta", OutputFolder+"/medaka2/calls_to_draft.bam"], stdout=subprocess.PIPE)
+    subprocess.call(['bcftools', 'call', '-mv', '-Oz', '-o', OutputFolder+'/call.vcf.gz', '--ploidy', '1'], stdin=bcf_pileup_cmd.stdout)
+
+    subprocess.call(['bcftools', 'index', OutputFolder+'/call.vcf.gz'])
+
+    f = open(OutputFolder+"/final_scaffold.fasta", "w")
+    cat_pileup_cmd = subprocess.Popen(['cat', OutputFolder+"/gapfixed.fasta" ], stdout=subprocess.PIPE)
+    subprocess.call(['bcftools', 'consensus', OutputFolder+'/call.vcf.gz', '-H', '1'], stdin=cat_pileup_cmd.stdout, stdout=f)
+    f.close()
 
 
 if __name__ == "__main__":
